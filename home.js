40c9936fa7ee6b4a6bc0cf36b1c6fd572d9bb561
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-22 15:43:44 +0300

    Upload Project

--- home.js
+++ home.js
@@ -20,7 +20,7 @@ function getPosts(page = 1) {
     const posts = response.data.data;
     lastPage = response.data.meta.last_page;
     // If first page, clear posts. Otherwise, append.
-    if (page === 1) {
+    if (page === 1 && document.getElementById("posts") != null) {
       document.getElementById("posts").innerHTML = "";
     }
     for (const post of posts) {
@@ -40,7 +40,7 @@ function getPosts(page = 1) {
           />
           <b>${author.username}</b>
         </div>
-        <div class="card-body">
+        <div class="card-body" onclick="postClicked(${post.id})" style="cursor: pointer;">
           <img src="${post.image}" alt="Post Image" class="w-100" />
           <h6 class="mt-1" style="color: rgb(193, 193, 193)">
             ${post.created_at}
@@ -67,7 +67,7 @@ function getPosts(page = 1) {
         </div>
       </div>
     `;
-      document.getElementById("posts").innerHTML += content;
+      
       const postTagsId = document.getElementById(`postTags-${post.id}`);
 
       for (const tag of post.tags) {
@@ -80,6 +80,9 @@ function getPosts(page = 1) {
         </button>
       `;
         postTagsId.innerHTML += tagsContent;
+      }
+      if (document.getElementById("posts") != null) {
+        document.getElementById("posts").innerHTML += content;
       }
     }
   });
@@ -99,14 +102,18 @@ function setUpUI() {
     loginBtn.style.display = "block";
     registerBtn.style.display = "block";
     logoutBtn.style.display = "none";
-    addPostBtn.style.display = "none";
+    if (addPostBtn != null) {
+      addPostBtn.style.display = "none";
+    }
     profileImage.style.display = "none";
     navUsername.style.display = "none";
   } else {
     loginBtn.style.display = "none";
     registerBtn.style.display = "none";
     logoutBtn.style.display = "block";
-    addPostBtn.style.display = "block";
+    if (addPostBtn != null) {
+      addPostBtn.style.display = "block";
+    }
     profileImage.src = user.profile_image;
     navUsername.innerHTML = user.username;
   }
@@ -287,6 +294,96 @@ function getCurrentUser() {
     user = JSON.parse(storageUser);
   }
   return user;
+}
+
+function postClicked(postId) {
+  window.location.href = `postDetails.html?id=${postId}`;
+}
+
+const serchParams = new URLSearchParams(window.location.search);
+const postId = serchParams.get("id");
+if (postId != null) {
+  getPostDetails(postId);
+}
+
+ function getPostDetails(postId) {
+  const url = baseURL + "/posts/" + postId;
+  const postTitleElement = document.getElementById("postDetailsUsername");
+  axios.get(url).then((response) => {
+    const post = response.data.data;
+    const author = post.author;
+    const comments = post.comments;
+    let postTitle = "";
+    if (post.title != null) {
+      postTitle = post.title;
+      postTitleElement.innerHTML = author.username;
+    }
+    let commentsContent = ``;
+    for (const comment of comments) {
+      commentsContent += `
+      <div class="p-3" style="background-color: rgb(187, 187, 187);">
+        <div class="d-flex align-items-center">
+          <img
+            src="${comment.author.profile_image}"
+            alt="Profile Image"
+            class="rounded-circle border border-2"
+            style="width: 40px; height: 40px"
+          />
+          <b class="ms-2">${comment.author.username}</b>
+        </div>
+
+        <div>
+          ${comment.body}
+        </div>
+      </div>
+    `;
+    }
+    let content = `
+    <div class="card shadow rounded">
+      <div class="card-header">
+        <img
+          src="${author.profile_image}"
+          alt="Profile Image"
+          class="rounded-circle border border-2"
+          style="width: 40px; height: 40px"
+        />
+        <b>${author.username}</b>
+      </div>
+      <div class="card-body">
+        <img src="${post.image}" alt="Post Image" class="w-100" />
+        <h6 class="mt-1" style="color: rgb(193, 193, 193)">
+          ${post.created_at}
+        </h6>
+        <h5>${postTitle}</h5>
+        <p>${post.body}</p>
+        <hr />
+        <div style="display: flex; align-items: center;">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-pen"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"
+            />
+          </svg>
+          <span style="display: flex; align-items: center;">
+            (${post.comments_count}) Comments
+            <span style="margin-left: 10px; display: flex; gap: 5px;" id="postTags-${post.id}"></span>
+          </span>
+        </div>
+      </div>
+
+      <div id="comments">
+        ${commentsContent}
+      </div>
+    </div>
+  `;
+    document.getElementById("postDetails").innerHTML = content;
+  });
 }
 
 
